5fc39f2963fc8f4d0b8309c3d31ef90ed1460b6c
Dean <dean.yang02@gmail.com>
2013-10-29 16:02:55 -0700

    initial push

---
+++ app/assets/javascripts/collections/post.js
@@ -0,0 +1,4 @@
+Journal.Collections.Post = Backbone.Collection.extend({
+	model: Journal.Models.Post
+	url: '/posts'
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journal::Application.config.secret_token = 'd47e4c742436c397c4ab0cea6d54fc9d910b30dec707e9de66ef8242f448e23d48444035bc88f64643dfb6d7fa0749eebe52128c8ab125ec6d0478bf387d444f'
